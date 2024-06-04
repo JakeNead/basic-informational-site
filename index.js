f025a044dc71941f599c1ed99d4d463ff03b2048
Jake Nead <jakenead@gmail.com>
2024-06-04 15:48:24 -0700

    Routes serve correct pages

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
 const fs = require("fs");
 const http = require("http");
 
-const server = http.createServer((req, res) => {});
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "text/html");
+
+  let path = "./";
+  console.log(req.url);
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      res.statusCode = 200;
+      break;
+    case "/about.html":
+      path += "about.html";
+      res.statusCode = 200;
+      break;
+    case "/contact-me":
+      path += "contact-me.html";
+      res.statusCode = 200;
+      break;
+    default:
+      path += "404.html";
+      res.statusCode = 404;
+      break;
+  }
+
+  fs.readFile(path, (err, data) => {
+    console.log(path);
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.write(data);
+      res.end();
+    }
+  });
+});
 
 server.listen(8080);
